a7c6dbd8b5f37166bf647ce05d7a2d6053aaa81e
Alexander Royal <alexander.de.royal@gmail.com>
2023-07-16 00:57:27 +0545

    section 2 started

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print("Hello World")
+# print("Hello World")
+
+# name = input("What is your name?")
+# print(name)
+
+# name = "Jack"
+# print(name)
+
+# name = "Angela"
+# print(name)
+
+# print(len(input("What is your name?")))
+
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+
+# print("Welcome to the band name generator")
+# city = input("Which city did you grew up in?")
+# pet = input("What is the name of your pet?")
+# print(city + " " + pet);
+
+# #Strings
+# print("Hello"[0])
+#
+# print("123" + "345")
+#
+# #Integer
+# print(123+345)
+#
+# #Float
+# print(3.141 + 98.181)
+#
+# #Boolean
+#
+# num_char = len(input("What is your name?"))
+# print("Your name has " + num_char + " characters.")
+
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# # print(type(num_char))
+# print("Your name has " + new_num_char + " characters.")
+
+a = 123
+print(type(a))
+
+
